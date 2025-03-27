afeb9748853bd64db838c99d13cd453d4ae3b32e
qiv1ne <sergeivanov@gmail.com>
2025-03-27 19:48:11 +0300

    create handle request func exclude logic from Start function

--- server/server.go
+++ server/server.go
@@ -47,12 +47,36 @@ func (s *Server) Start() error {
 			slog.Error("read from connection", "err", err)
 		}
 
-		message, err := protocol.DecodeRequest(data)
-		if err != nil {
-			slog.Error("decode request", "err", err)
-		}
-
-		slog.Info("message processed", "message", message)
-
+func (s Server) handleRequest(data []byte, conn *net.UDPConn, addr *net.UDPAddr) {
+	message, err := protocol.DecodeRequest(data)
+	if err != nil {
+		slog.Error("decode request", "err", err)
+		return
 	}
+
+	slog.Info("message processed", "head", message.Head)
+
+	for _, question := range message.Questions {
+		slog.Info("searching for domain", "domain", question.Domain)
+		answers, err := s.db.GetResourceRecord(question.Domain)
+		if err != nil {
+			slog.Error("get RR from db", "err", err)
+			return
+		}
+		message.Answers = append(message.Answers, answers...)
+	}
+
+	answer, err := protocol.EncodeResponse(message)
+	if err != nil {
+		slog.Error("encode response", "err", err)
+		return
+	}
+	slog.Info("response encoded")
+
+	_, err = conn.WriteToUDP(answer, addr)
+	if err != nil {
+		slog.Error("write response to conn", "err", err)
+		return
+	}
+	slog.Info("request writed to client")
 }
