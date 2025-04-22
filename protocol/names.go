da31388cd6e11867eed301ea3e329a615eb1f591
prionis <sergeivanov827@gmail.com>
2025-04-22 11:49:05 +0300

    fix bugs of read/write domain names in buffer

--- protocol/names.go
+++ protocol/names.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func ValidateDomain(domain string) error {
+func ValidateDomainName(domain string) error {
 	if len(domain) > 255 {
 		return errors.New("domain name is too long")
 	}
@@ -38,44 +38,79 @@ func ValidateLabel(label string) error {
 	return nil
 }
 
-func DecodeDomain(buffer *bytes.Buffer) (string, error) {
-	domainParts := make([]string, 0)
+func ReadDomainName(buffer *bytes.Buffer) (string, error) {
+	var domainName string
 
-	// RFC 1035: "a domain name represented as a sequence of labels, where
-	// each label consists of a length octet followed by that
-	// number of octets."
 	b, err := buffer.ReadByte()
-	if err != nil {
-		return "", err
-	}
 
 	for ; b != 0 && err == nil; b, err = buffer.ReadByte() {
-		domainBytes := buffer.Next(int(b))
-		domainParts = append(domainParts, string(domainBytes))
+		labelLength := int(b)
+		labelBytes := buffer.Next(labelLength)
+		labelName := string(labelBytes)
+
+		if len(domainName) == 0 {
+			domainName = labelName
+		} else {
+			domainName += "." + labelName
+		}
 	}
-	domain := strings.Join(domainParts, ".")
-	return domain, ValidateDomain(domain)
+
+	return domainName, err
 }
 
-func EncodeDomain(domain string, buffer *bytes.Buffer) error {
-	if err := ValidateDomain(domain); err != nil {
-		return err
+//	func DecodeDomain(buffer *bytes.Buffer) (string, error) {
+//		domainParts := make([]string, 0)
+//
+//		// RFC 1035: "a domain name represented as a sequence of labels, where
+//		// each label consists of a length octet followed by that
+//		// number of octets."
+//		b, err := buffer.ReadByte()
+//		if err != nil {
+//			return "", err
+//		}
+//
+//		for ; b != 0 && err == nil; b, err = buffer.ReadByte() {
+//			domainBytes := buffer.Next(int(b))
+//			domainParts = append(domainParts, string(domainBytes))
+//		}
+//		domain := strings.Join(domainParts, ".")
+//		return domain, ValidateDomain(domain)
+//	}
+func WriteDomainName(domainName string, responseBuffer *bytes.Buffer) error {
+	labels := strings.Split(domainName, ".")
+
+	for _, label := range labels {
+		labelLength := len(label)
+		labelBytes := []byte(label)
+
+		responseBuffer.WriteByte(byte(labelLength))
+		responseBuffer.Write(labelBytes)
 	}
 
-	err := buffer.WriteByte(byte(len(domain)))
-	if err != nil {
-		return err
-	}
+	err := responseBuffer.WriteByte(byte(0))
 
-	_, err = buffer.WriteString(domain)
-	if err != nil {
-		return err
-	}
-
-	err = buffer.WriteByte(0)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
+
+// func EncodeDomain(domain string, buffer *bytes.Buffer) error {
+// 	if err := ValidateDomain(domain); err != nil {
+// 		return err
+// 	}
+//
+// 	err := buffer.WriteByte(byte(len(domain)))
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	_, err = buffer.WriteString(domain)
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	err = buffer.WriteByte(0)
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	return nil
+// }
