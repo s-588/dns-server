add2000a3d81515368b4041dee9aaada4de9d938
prionis <segeivanov@gmail.com>
2025-05-06 18:54:50 +0300

    add flag parsing

--- main.go
+++ main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
-	"log/slog"
-	"os"
+	"flag"
 
-	"github.com/prionis/dns-server/server"
+	"github.com/prionis/dns-server/ui"
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdin, nil))
-	config := []server.Option{
-		server.SetPort("1053"),
-		server.WithLogger(logger),
-	}
-	s, err := server.NewServer(config...)
-	if err != nil {
-		logger.Error("can't create new server", "config", config, "error", err)
-		os.Exit(1)
-	}
+	flagServer := flag.Bool("server", false, "start DNS server")
+	flagTUI := flag.Bool("tui", false, "show simple text interface")
+	flagListLog := flag.Bool("list-log", false, "show all logs in list view")
+	flagListRR := flag.Bool("list-rr", false, "show all resource records in list view")
+	flagAddRR := flag.String("add", "", "add new resource record. Accept 5 parameters (type,class,domain,data,TimeToLive). First 4 is necessary")
+	flagDelRR := flag.Int64("del", -1, "delete resource record. Accept ID of resource record to delete")
 
-	if err := s.Start(); err != nil {
-		logger.Error("can't start server", "config", config, "error", err)
+	flag.Parse()
+	ui.CheckArgs(flagServer, flagTUI, flagListLog, flagListRR, flagAddRR, flagDelRR)
+
+	switch {
+	case *flagAddRR != "":
+		ui.AddRR(*flagAddRR)
+	case *flagDelRR != -1:
+		ui.DelRR(*flagDelRR)
+	case *flagServer:
+		ui.StartServer()
+	case *flagTUI:
+		ui.StartTUI()
+	case *flagListLog:
+		ui.PrintLogList()
+	case *flagListRR:
+		ui.PrintRRList()
+	default:
+		flag.Usage()
 	}
 }
