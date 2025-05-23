f89815f55a48712a247ece0c504d4d3191d09fbc
prionis <segeivanov@gmail.com>
2025-05-23 15:51:48 +0300

    add window adjustment in delete model

--- ui/tui/model.go
+++ ui/tui/model.go
@@ -114,6 +114,7 @@ func NewModel() (model, error) {
 		logTable: logTable,
 
 		popup:      popup.NewPopupModel(),
+		deletePage: crud.NewDeleteModel(nil, &db, w, h),
 		addPage:    crud.NewAddModel(&db, w, h),
 		db: &db,
 	}, nil
